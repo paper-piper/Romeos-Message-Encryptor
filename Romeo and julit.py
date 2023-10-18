eaa4286369e7a50e978f936095038a8c8f37df76
yoni kisho <yonatanreichert@gmail.com>
2023-10-18 12:33:31 +0300

    Fixed message path problem (used encrypted_msg.txt instead of absolute path)

--- Romeo and julit.py
+++ Romeo and julit.py
@@ -1,12 +1,13 @@
 import sys
 import logging
+import os
 
 
 # Configure the logging module
 logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Global variables
-MESSAGES_FILE_PATH = r"C:\Users\yonat\Downloads\Messages.txt"
+MESSAGES_FILE_PATH = r"encrypted_msg.txt"
 DECRYPTION_DICTIONARY = {'56': 'A', '45': 'J', '64': 'S', '13': 'b', '32': 'k', '91': 't', '100': '.', '57': 'B', '46': 'K', '65': 'T', '14': 'c', '33': 'l', '92': 'u', '101': ';', '58': 'C', '47': 'L', '66': 'U', '15': 'd', '34': 'm', '93': 'v', '102': '‘', '59': 'D', '48': 'M', '67': 'V', '16': 'e', '35': 'n', '94': 'w', '103': '?', '40': 'E', '49': 'N', '68': 'W', '17': 'f', '36': 'o', '95': 'x', '104': '!', '41': 'F', '60': 'O', '69': 'X', '18': 'g', '37': 'p', '96': 'y', '105': ':', '42': 'G', '61': 'P', '10': 'Y', '19': 'h', '38': 'q', '97': 'z', '43': 'H', '62': 'Q', '11': 'Z', '30': 'i', '39': 'r', '98': ' ', '44': 'I', '63': 'R', '12': 'a', '31': 'j', '90': 's', '99': ','}
 ENCRYPTION_DICTIONARY = {v: k for k, v in DECRYPTION_DICTIONARY.items()}
 
@@ -42,7 +43,8 @@ def decrypt_message():
             logging.info(f"Decrypted the message: '{decrypted_message}'")
             return decrypted_message
     except FileNotFoundError:
-        logging.error(f"Error: File not found at path {MESSAGES_FILE_PATH}")
+        logging.info(f"File not found at path {MESSAGES_FILE_PATH}, so returned empty string")
+        return ""
     except Exception as e:
         logging.error(f"An unknown error occurred: {str(e)}")
 
